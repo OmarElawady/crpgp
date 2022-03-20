cc12e35482541a42cb3ddf8f3e201ec660efb0eb
OmarElawady <omarelawady1998@gmail.com>
2022-03-20 17:28:17 +0200

    add detachable signature support

--- src/signature.rs
+++ src/signature.rs
@@ -7,6 +7,7 @@ use pgp;
 
 use pgp::de::Deserialize;
 use pgp::ser::Serialize;
+use pgp::Deserializable;
 
 use std::ptr;
 
@@ -46,6 +47,60 @@ pub extern "C" fn signature_deserialize(
     let signature = pgp::Signature::from_slice(pgp::types::Version::Old, signature_vec.as_slice());
     match signature {
         Ok(v) => Box::into_raw(Box::new(v)),
+        Err(e) => {
+            update_last_error(e.to_string());
+            ptr::null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn signature_to_armored_string(
+    signature: *mut pgp::Signature,
+    output_len: *mut size_t,
+) -> *mut c_char {
+    if signature.is_null() {
+        update_last_error(Box::new("signature can't be null".into()));
+        return ptr::null_mut();
+    }
+    let signature = unsafe { &*signature };
+    let s = pgp::StandaloneSignature::new(signature.clone()).to_armored_string(None);
+    match s {
+        Ok(v) => {
+            let p = string_to_bytes(&v, output_len);
+            if let Err(e) = p {
+                update_last_error(Box::new(e.into()));
+                return ptr::null_mut();
+
+            }
+            p.unwrap() // safe unwrap
+
+        }
+        Err(e) => {
+            update_last_error(e.to_string());
+            ptr::null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn signature_from_string(
+    signature_bytes: *mut c_char,
+    len: size_t,
+) -> *mut pgp::Signature {
+    if signature_bytes.is_null() {
+        update_last_error(Box::new("signature bytes can't be null".into()));
+        return ptr::null_mut();
+    }
+    let signature = bytes_to_string(signature_bytes, len);
+    if let Err(e) = signature {
+        update_last_error(e);
+        return ptr::null_mut();
+    }
+    let signature = signature.unwrap(); // safe unwrap
+    let signature = pgp::StandaloneSignature::from_string(&signature);
+    match signature {
+        Ok(v) => Box::into_raw(Box::new(v.0.signature)),
         Err(e) => {
             update_last_error(e.to_string());
             ptr::null_mut()
--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,41 @@
+use std::ffi::CStr;
+use std::ffi::CString;
 use crate::err::*;
 use libc::{c_char, size_t};
+use std::ptr;
 
 pub fn vec_from_ptr<T: Clone>(bytes: *mut T, len: size_t) -> Vec<T> {
-    unsafe {
-        let v = Vec::from_raw_parts(bytes, len, len);
-        let res = v.clone();
-        std::mem::forget(v);
-        res
-    }
+    let v = unsafe { Vec::from_raw_parts(bytes, len, len) };
+    let res = v.clone();
+    std::mem::forget(v);
+    res
 }
+
+pub fn ptr_from_vec<T: Clone>(mut bytes: Vec<T>, output_len: *mut size_t) -> *mut T {
+    bytes.shrink_to_fit();
+    unsafe { *output_len = bytes.len() }
+    let ptr = bytes.as_mut_ptr();
+    std::mem::forget(bytes);
+    ptr
+}
+
+pub fn string_to_bytes(s: &str, output_len: *mut size_t) -> Result<*mut c_char, String> {
+    unsafe { *output_len = s.len() + 1 };
+    let s = CString::new(s);
+    if let Err(e) = s {
+        return Err(e.to_string())
+    }
+    Ok(s.unwrap().into_raw())
+}
+
+pub fn bytes_to_string(s: *mut c_char, len: size_t) -> Result<String, String> {
+    let s = unsafe { CStr::from_ptr(s) }.to_str();
+    if let Err(e) = s {
+        return Err(e.to_string())
+    }
+    Ok(s.unwrap().into()) // safe unwrap
+}
+
 #[no_mangle]
 pub extern "C" fn ptr_free(ptr: *mut u8) -> c_char {
     if ptr.is_null() {
